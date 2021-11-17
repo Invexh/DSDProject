f2b524acba0d4e523c3056aa3a1aea82ad99b468
SamuelSylvester <73556292+SamuelSylvester@users.noreply.github.com>
2021-11-17 09:15:27 -0600

    Pushing Aliens file
    -added aliens.vhd
    -added alien components

---
+++ aliens.vhd
@@ -0,0 +1,592 @@
+-- Aliens for the defender game
+
+------------- Aliens Spawning on RNG -------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.all;
+
+entity AlienRNG is 
+	port(
+		max10_clk 			: in std_logic;
+		RNG 					: in std_logic_vector(9 downto 0);
+		alive					: inout std_logic := '0';
+		size 					: inout unsigned(2 downto 0);
+		color					: out std_logic_vector(11 downto 0);
+		x_pos					: inout integer := 641;
+		y_pos					: inout integer := 240;
+		min_period			: in integer := 5;
+		RNG_bit_map			: in std_logic_vector(9 downto 0);
+		ship_x				: in integer;
+		ship_y 				: in integer
+	);
+end entity;
+
+architecture beh of AlienRNG is
+
+	signal RNG_instance 			: std_logic_vector(9 downto 0);
+	signal i 						: std_logic := '0';
+	signal movement_clock 		: std_logic;
+	
+begin
+	initialize : process(alive)
+	begin
+		if(rising_edge(alive)) then
+			if(i = '0') then				-- save RNG instance
+				RNG_instance <= RNG;
+				i <= '1';
+			elsif(falling_edge(alive)) then
+				i <= '0';
+			end if;
+		
+			size(2 downto 0) <= unsigned(RNG_instance(6 downto 4));
+			color <= "101000001111";
+		end if;
+	end process;
+		
+	move_clock : process (max10_clk)
+	variable movement_counter : integer := 0;
+	begin
+		if(rising_edge(max10_clk)) then
+			movement_counter := movement_counter + 1;		
+		end if;
+		
+		if (movement_counter <= 100000) then
+			movement_clock <= NOT movement_clock;
+			movement_counter := 0;
+		end if;
+
+	end process;
+	
+	movement : process (movement_clock)
+	variable floaty : integer := 0;
+	variable fast : integer := 0;
+	variable slow : integer := 0;
+	variable diagonalSpeed : integer := 0;
+	variable diagonalDir : integer := 0;
+	
+	variable y_max : INTEGER := 67;
+	variable y_min : INTEGER := 413;
+	variable x_max : INTEGER := 640;
+	variable x_min : INTEGER := 25;
+	
+	begin
+		if(rising_edge(movement_clock) AND alive = '1') then
+			case size is
+				when "011" | "000" =>	-- floaty
+					if(floaty > 500) then
+						if(RNG(9) = '1') then
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (RNG(6) = '1') then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;
+						floaty := 0;
+					else
+						floaty := floaty + 1;
+					end if;
+					
+					
+				when "100" | "101" =>	-- diagonal
+					if(diagonalSpeed > 10) then
+						if(diagonalDir = 0) then	--up left
+							if(x_pos = x_min) then
+								diagonalDir := 3;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 1;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+							
+						elsif(diagonalDir = 1) then	-- down left
+							if(x_pos = x_min) then
+								diagonalDir := 2;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_min) then
+								diagonalDir := 0;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+							
+						elsif(diagonalDir = 2) then	-- down right
+							if(x_pos = x_max) then
+								diagonalDir := 1;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_min) then
+								diagonalDir := 3;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+						else									-- up right
+							if(x_pos = x_max) then
+								diagonalDir := 0;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 2;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+						end if;
+						diagonalSpeed := 0;
+					else
+						diagonalSpeed := diagonalSpeed + 1;	
+					end if;
+					
+					
+					
+				when "001" | "010" =>	-- horizontal fast
+					if(fast > 5) then
+						x_pos <= x_pos - 1;
+						fast := 0;
+					else
+						fast := fast + 1;
+					end if;
+					
+				when "110" | "111" =>	-- slowly approach player
+					if (slow > 20) then
+						if (ship_x > x_pos) then
+							x_pos <= x_pos + 1;
+						else
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (ship_y > y_pos) then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;						
+						slow := 0;
+					else
+						slow := slow + 1;
+					end if;
+			end case;
+		--else								--default starting point
+		--	x_pos <= 640;
+		--	y_pos <= 240;
+		end if;
+	end process;
+	
+	spawning : process (max10_clk)
+	variable timeSinceLastSpawn : unsigned(31 downto 0) := (OTHERS => '0');
+	begin
+		timeSinceLastSpawn := timeSinceLastSpawn + 1;
+		if( (RNG AND RNG_bit_map) = RNG_bit_map AND alive = '0' AND timeSinceLastSpawn > (50000000*min_period)) then
+			alive <= '1';
+			timeSinceLastSpawn := (OTHERS => '0');
+		end if;
+	
+	
+	end process;
+	
+	
+	
+end architecture;
+
+------------- Aliens Spawning on Constant Timer -----------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.all;
+
+entity AlienTimer is 
+	port(
+		max10_clk 			: in std_logic;
+		RNG 					: in std_logic_vector(9 downto 0);
+		alive					: inout std_logic := '0';
+		size 					: inout unsigned(2 downto 0);
+		color					: out std_logic_vector(11 downto 0);
+		x_pos					: inout integer := 641;
+		y_pos					: inout integer := 153;
+		period_seconds 	: in integer;
+		ship_x				: in integer;
+		ship_y 				: in integer
+	);
+end entity;
+
+architecture beh of AlienTimer is
+
+	signal RNG_instance 			: std_logic_vector(9 downto 0);
+	signal i 						: std_logic;
+	signal movement_clock 		: std_logic;
+	
+begin
+	initialize : process(alive)
+	begin
+		if(rising_edge(alive)) then
+			if(i = '0') then				-- save RNG instance
+				RNG_instance <= RNG;
+				i <= '1';
+			elsif(falling_edge(alive)) then
+				i <= '0';				
+			end if;
+		
+			size(2 downto 0) <= unsigned(RNG_instance(9 downto 7));
+			color <= "000011110000";
+		end if;
+	end process;	
+
+	move_clock : process (max10_clk)
+	variable movement_counter : integer := 0;
+	begin
+		if(rising_edge(max10_clk)) then
+			movement_counter := movement_counter + 1;		
+		end if;
+		
+		if (movement_counter <= 100000) then
+			movement_clock <= NOT movement_clock;
+			movement_counter := 0;
+		end if;
+	end process;
+	
+	movement : process (movement_clock)
+	variable floaty : integer := 0;
+	variable fast : integer := 0;
+	variable slow : integer := 0;
+	variable diagonalSpeed : integer := 0;
+	variable diagonalDir : integer := 0;
+	
+	variable y_max : INTEGER := 67;
+	variable y_min : INTEGER := 413;
+	variable x_max : INTEGER := 640;
+	variable x_min : INTEGER := 25;
+	
+	begin
+		if(rising_edge(movement_clock) AND alive = '1') then
+			case size is
+				when "011"|"000" =>	-- floaty
+					if(floaty > 500) then
+						if(RNG(5) = '1') then
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (RNG(3) = '1') then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;
+						floaty := 0;
+					else
+						floaty := floaty + 1;
+					end if;
+					
+					
+				when "100"|"101" =>	-- diagonal
+					if(diagonalSpeed > 10) then
+						if(diagonalDir = 0) then	--up left
+							if(x_pos = x_min) then
+								diagonalDir := 3;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 1;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+							
+						elsif(diagonalDir = 1) then	-- down left
+							if(x_pos = x_min) then
+								diagonalDir := 2;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_min) then
+								diagonalDir := 0;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+							
+						elsif(diagonalDir = 2) then	-- down right
+							if(x_pos = x_max) then
+								diagonalDir := 1;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_min) then
+								diagonalDir := 3;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+						else									-- up right
+							if(x_pos = x_max) then
+								diagonalDir := 0;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 2;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+						end if;
+						diagonalSpeed := 0;
+					else
+						diagonalSpeed := diagonalSpeed + 1;	
+					end if;
+					
+					
+					
+				when "001"|"010" =>	-- horizontal fast
+					if(fast > 5) then
+						x_pos <= x_pos - 1;
+						fast := 0;
+					else
+						fast := fast + 1;
+					end if;
+					
+				when "110"|"111" =>	-- slowly approach player
+					if (slow > 20) then
+						if (ship_x > x_pos) then
+							x_pos <= x_pos + 1;
+						else
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (ship_y > y_pos) then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;						
+						slow := 0;
+					else
+						slow := slow + 1;
+					end if;
+			end case;
+		end if;
+	end process;
+	
+	spawning : process (max10_clk)
+	variable timeSinceLastSpawn : unsigned(31 downto 0)  := (OTHERS => '0');
+	begin
+		timeSinceLastSpawn := timeSinceLastSpawn + 1;
+		if(alive = '0' AND timeSinceLastSpawn > (50000000*period_seconds)) then
+			alive <= '1';
+			timeSinceLastSpawn := (OTHERS => '0');
+		end if;	
+	end process;
+end architecture;
+
+------------- Aliens Spawning on Score based timer -------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.all;
+
+entity AlienScoreTimer is 
+	port(
+		max10_clk 				: in std_logic;
+		RNG 						: in std_logic_vector(9 downto 0);
+		alive						: inout std_logic := '0';
+		size 						: inout unsigned(2 downto 0);
+		color						: out std_logic_vector(11 downto 0);
+		x_pos						: inout integer := 641; 
+		y_pos						: inout integer := 326;
+		max_period_seconds	: in integer;
+		min_period_seconds	: in integer;
+		current_score			: in integer;
+		ship_x				: in integer;
+		ship_y 				: in integer
+	);
+end entity;
+
+architecture beh of AlienScoreTimer is
+
+	signal RNG_instance 			: std_logic_vector(9 downto 0);
+	signal i 						: std_logic;
+	signal movement_clock 		: std_logic;
+	
+begin
+	initialize : process(alive)
+	begin
+		if(rising_edge(alive)) then
+			if(i = '0') then				-- save RNG instance
+				RNG_instance <= RNG;
+				i <= '1';
+			elsif(falling_edge(alive)) then
+				i <= '0';			
+			end if;
+		
+			size(2 downto 0) <= unsigned(RNG_instance(3 downto 1));
+			color <= "000000001111";
+		end if;
+	end process;
+	
+	move_clock : process (max10_clk)
+	variable movement_counter : integer := 0;
+	begin
+		if(rising_edge(max10_clk)) then
+			movement_counter := movement_counter + 1;		
+		end if;
+		
+		if (movement_counter <= 100000) then
+			movement_clock <= NOT movement_clock;
+			movement_counter := 0;			
+		end if;
+	end process;
+	
+	movement : process (movement_clock)
+	variable floaty : integer := 0;
+	variable fast : integer := 0;
+	variable slow : integer := 0;
+	variable diagonalSpeed : integer := 0;
+	variable diagonalDir : integer := 0;
+	
+	variable y_max : INTEGER := 67;
+	variable y_min : INTEGER := 413;
+	variable x_max : INTEGER := 640;
+	variable x_min : INTEGER := 25;
+	
+	begin
+		if(rising_edge(movement_clock) AND alive = '1') then
+			case size is
+				when "011"|"000" =>	-- floaty
+					if(floaty > 500) then
+						if(RNG(8) = '1') then
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (RNG(0) = '1') then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;
+						floaty := 0;
+					else
+						floaty := floaty + 1;
+					end if;
+					
+					
+				when "100"|"101" =>	-- diagonal
+					if(diagonalSpeed > 10) then
+						if(diagonalDir = 0) then	--up left
+							if(x_pos = x_min) then
+								diagonalDir := 3;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 1;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+							
+						elsif(diagonalDir = 1) then	-- down left
+							if(x_pos = x_min) then
+								diagonalDir := 2;
+							else
+								x_pos <= x_pos - 1;
+							end if;
+							
+							if(y_pos = y_min) then
+								diagonalDir := 0;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+							
+						elsif(diagonalDir = 2) then	-- down right
+							if(x_pos = x_max) then
+								diagonalDir := 1;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_min) then
+								diagonalDir := 3;
+							else
+								y_pos <= y_pos + 1;
+							end if;
+							
+						else									-- up right
+							if(x_pos = x_max) then
+								diagonalDir := 0;
+							else
+								x_pos <= x_pos + 1;
+							end if;	
+								
+							if(y_pos = y_max + (to_integer(size)+1)*8) then
+								diagonalDir := 2;
+							else
+								y_pos <= y_pos - 1;
+							end if;
+						end if;
+						diagonalSpeed := 0;
+					else
+						diagonalSpeed := diagonalSpeed + 1;	
+					end if;
+					
+					
+					
+				when "001"|"010" =>	-- horizontal fast
+					if(fast > 5) then
+						x_pos <= x_pos - 1;
+						fast := 0;
+					else
+						fast := fast + 1;
+					end if;
+					
+				when "110"|"111" =>	-- slowly approach player
+					if (slow > 20) then
+						if (ship_x > x_pos) then
+							x_pos <= x_pos + 1;
+						else
+							x_pos <= x_pos - 1;
+						end if;
+						
+						if (ship_y > y_pos) then
+							y_pos <= y_pos + 1;
+						else
+							y_pos <= y_pos - 1;
+						end if;						
+						slow := 0;
+					else
+						slow := slow + 1;
+					end if;
+			end case;
+		end if;
+	end process;
+	
+	spawning : process (max10_clk)
+	variable timeSinceLastSpawn : unsigned(31 downto 0)  := (OTHERS => '0');
+	variable numSpawns : integer := 0;
+	variable period : integer := max_period_seconds;
+	begin
+		timeSinceLastSpawn := timeSinceLastSpawn + 1;
+		if(period > min_period_seconds) then
+			period := max_period_seconds - numSpawns;
+		else
+			period := min_period_seconds;
+		end if;
+		
+		if(alive = '0' AND timeSinceLastSpawn > (50000000*period)) then
+			alive <= '1';
+			timeSinceLastSpawn := (OTHERS => '0');
+			numSpawns := numSpawns + 1;
+		end if;
+	end process;
+end architecture;
--- dsdproject.vhd
+++ dsdproject.vhd
@@ -145,12 +145,80 @@ ARCHITECTURE behavior OF dsdproject IS
 			PRNG10 : buffer std_logic_vector(9 downto 0)
 		);			
 	end component;
-BEGIN
+
+	-- Alien that spawns when timer and RNG conditions are met
+	component AlienRNG is 
+		port(
+			max10_clk 			: in std_logic := '0';
+			RNG 					: in std_logic_vector(9 downto 0) := "0000000000";
+			alive					: inout std_logic := '0';
+			size 					: out unsigned(2 downto 0);
+			color					: out std_logic_vector(11 downto 0);
+			x_pos					: inout integer := 640;
+			y_pos					: inout integer := 240;
+			min_period			: in integer := 5;
+			RNG_bit_map			: in std_logic_vector(9 downto 0) := "1111111111";
+			ship_x				: in integer := 0;
+			ship_y 				: in integer := 0
+		);
+	end component;
+	
+	-- Alien that spawns on a constant timer
+	component AlienTimer is 
+		port(
+			max10_clk 			: in std_logic := '0';
+			RNG 					: in std_logic_vector(9 downto 0) := "0000000000";
+			alive					: inout std_logic := '0';
+			size 					: inout unsigned(2 downto 0);
+			color					: out std_logic_vector(11 downto 0);
+			x_pos					: inout integer;
+			y_pos					: inout integer;
+			period_seconds 	: in integer;
+			ship_x				: in integer := 0;
+			ship_y 				: in integer := 0
+		);
+	end component;
+	
+	-- Alien that spawns on a timer that gets faster as the game progresses
+	component AlienScoreTimer is 
+		port(
+			max10_clk 				: in std_logic := '0';
+			RNG 						: in std_logic_vector(9 downto 0) := "0000000000";
+			alive						: inout std_logic := '0';
+			size 						: inout unsigned(2 downto 0);
+			color						: out std_logic_vector(11 downto 0);
+			x_pos						: inout integer;
+			y_pos						: inout integer;
+			max_period_seconds	: in integer;
+			min_period_seconds	: in integer;
+			current_score			: in integer;
+			ship_x					: in integer;
+			ship_y 					: in integer
+		);
+	end component;	
+	
+	
+	BEGIN
 	clkfucka <= clock_x;
 	clkfuckb <= clock_y;
 
 	U0 : ADXL345_controller port map('1', max10_clk, open, data_x, data_y, data_z, GSENSOR_SDI, GSENSOR_SDO, GSENSOR_CS_N, GSENSOR_SCLK);
 	U1 : RNG10 port map(reset_RNG, '0', max10_clk, RNG);
+	
+-------- Alien AIs ---------------------------------------------------------------------------------	
+	U02 : AlienRNG port map(max10_clk, RNG, alien(0).alive, alien(0).size, alien(0).color, alien(0).x, alien(0).y, 11, "1101111010", ship_x, ship_y);
+	U03 : AlienRNG port map(max10_clk, RNG, alien(1).alive, alien(1).size, alien(1).color, alien(1).x, alien(1).y, 20, "0110110111", ship_x, ship_y);
+	U04 : AlienRNG port map(max10_clk, RNG, alien(2).alive, alien(2).size, alien(2).color, alien(2).x, alien(2).y, 29, "1011101101", ship_x, ship_y);
+	U05 : AlienRNG port map(max10_clk, RNG, alien(3).alive, alien(3).size, alien(3).color, alien(3).x, alien(3).y, 35, "0011011111", ship_x, ship_y);
+	U06 : AlienTimer port map(max10_clk, RNG, alien(4).alive, alien(4).size, alien(4).color, alien(4).x, alien(4).y, 10, ship_x, ship_y);
+	U07 : AlienTimer port map(max10_clk, RNG, alien(5).alive, alien(5).size, alien(5).color, alien(5).x, alien(5).y, 17, ship_x, ship_y);
+	U08 : AlienTimer port map(max10_clk, RNG, alien(6).alive, alien(6).size, alien(6).color, alien(6).x, alien(6).y, 23, ship_x, ship_y);
+	U09 : AlienTimer port map(max10_clk, RNG, alien(7).alive, alien(7).size, alien(7).color, alien(7).x, alien(7).y, 13, ship_x, ship_y);
+	U10 : AlienScoreTimer port map(max10_clk, RNG, alien(8).alive, alien(8).size, alien(8).color, alien(8).x, alien(8).y, 30, 4, score, ship_x, ship_y);
+	U11 : AlienScoreTimer port map(max10_clk, RNG, alien(9).alive, alien(9).size, alien(9).color, alien(9).x, alien(9).y, 45, 5, score, ship_x, ship_y);
+	U12 : AlienScoreTimer port map(max10_clk, RNG, alien(10).alive, alien(10).size, alien(10).color, alien(10).x, alien(10).y, 50, 3, score, ship_x, ship_y);
+	U13 : AlienScoreTimer port map(max10_clk, RNG, alien(11).alive, alien(11).size, alien(11).color, alien(11).x, alien(11).y, 60, 7, score, ship_x, ship_y);
+	
 
 	PROCESS(disp_ena, row, column)
 		variable calcA : INTEGER;
