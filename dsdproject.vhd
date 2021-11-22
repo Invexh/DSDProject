4a962e21f3d84befbdda980a43b437314816e84b
SamuelSylvester <73556292+SamuelSylvester@users.noreply.github.com>
2021-11-21 19:20:47 -0600

    pew working

--- dsdproject.vhd
+++ dsdproject.vhd
@@ -96,7 +96,10 @@ ENTITY dsdproject IS
 	reset_RNG : IN STD_LOGIC;
 	
 	pause_toggle	: in std_logic;
-	shoot			: in std_logic
+	shoot			: in std_logic;
+	
+	buzzer1		: inout std_logic;
+	buzzer2 		: inout std_logic
 	
 	);
 END entity;
@@ -129,6 +132,11 @@ ARCHITECTURE behavior OF dsdproject IS
 	--OTHER--
 	signal RNG : std_logic_vector(9 downto 0);
 	signal pause 	: std_logic := '0';
+	
+	--AUDIO--
+	signal bz1_clk : 		std_logic := '0';
+	signal pew_sound :	std_logic := '0';
+	signal exp_sound :	std_logic := '0';
 	
 	-- Accelerometer component
 	component ADXL345_controller is port(	
@@ -543,4 +551,95 @@ ARCHITECTURE behavior OF dsdproject IS
 			end case;
 		END LOOP;
 	END PROCESS;
+	
+--BUZZER1 ------------------------------------
+
+--pew sound ----------------------------------
+	buzzer1_clock : process(clockWithPause)
+	variable C3_counter : integer := 0;
+	variable exp_clk_counter : integer := 0;
+	variable RNG_instance : integer := 1000000;		--generates random-ish clock for white noise	
+	begin
+		if(rising_edge(clockWithPause)) then	
+			if(exp_sound = '1') then
+				if (exp_clk_counter > RNG_instance ) then
+					bz1_clk <= not bz1_clk;
+					RNG_instance := 50000*(to_integer(unsigned(RNG(7 downto 3)))+1);
+					exp_clk_counter := 0;
+				else
+					bz1_clk <= bz1_clk;
+					exp_clk_counter := exp_clk_counter + 1;
+				end if;	
+			elsif(pew_sound = '1') then		
+				if (C3_counter > 38225 ) 	then	-- cycles to get frequency of 130.813 (C3) 
+					bz1_clk <= not bz1_clk;
+					C3_counter := 0;
+				else
+					bz1_clk <= bz1_clk;
+					C3_counter := C3_counter + 1;					
+				end if;
+			else
+				bz1_clk <= bz1_clk;
+			end if;
+		end if;	
+	end process;
+	
+	buzzer1_process : process(bz1_clk, clockWithPause, shoot )
+	variable pew_counter : integer := 0;
+	variable exp_counter : integer := 0;
+	variable sound_done  : std_logic := '0';
+	variable falling_edge_shoot : std_logic := '0';
+	begin
+		if(rising_edge(bz1_clk)) then	
+			if(exp_counter > 0) then		-- explosion
+				if(exp_counter < 50) then
+					buzzer1 <= not buzzer1;
+					exp_counter := exp_counter + 1;
+				else
+					exp_counter := 0;
+					sound_done := '1';
+				end if;			
+			elsif (pew_counter > 0) then					-- pew
+				if (pew_counter < 50) then
+					buzzer1 <= not buzzer1;
+					pew_counter := pew_counter + 1;
+				else
+					sound_done := '1';
+					pew_counter := 0;
+				end if;
+			elsif(exp_sound = '1') then
+				exp_counter := 1;
+			elsif(pew_sound = '1') then
+				pew_counter := 1;
+			else
+				buzzer1 <= buzzer1;
+				sound_done := '0';
+			end if;
+		end if;
+		
+		-- flags for sound creation
+		if(rising_edge(clockWithPause)) then
+			for i in 0 to 11 loop
+				if(alien(i).collision = '1') then
+					exp_sound <= '1';
+				elsif (sound_done = '1') then
+					exp_sound <= '0';	
+				else
+					exp_sound <= exp_sound;
+				end if;
+			end loop;
+			
+			for j in 0 to 19 loop
+				if (shoot = '0') then
+					pew_sound <='1';
+				elsif( sound_done = '1') then
+					pew_sound <= '0';
+				else
+					pew_sound <= pew_sound;
+				end if;
+			end loop;
+		end if;			
+			
+	end process;
+	
 END architecture;
